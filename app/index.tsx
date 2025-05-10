f7aab432d92e2fb470443ec203133717702687f5
Lufe04 <luisacarpintero01@gmail.com>
2025-05-10 13:12:53 -0500

    cambios

--- app/index.tsx
+++
@@ -1,8 +0,0 @@
-// app/index.tsx
-import { Redirect } from 'expo-router';
-
-export default function Index() {
-  // En cuanto el Root Layout termine de montarse,
-  // esto reemplazará la ruta “/” por “/auth”.
-  return <Redirect href="/auth" />;
-}
